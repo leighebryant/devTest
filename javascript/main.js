37ce4e7793508183e1abd8dfc76da1d649b9b6f6
Leigh <leighbryant@bellsouth.net>
2017-02-03 17:46:19 -0500

    safari bug fix and file clean up

--- javascript/main.js
+++ javascript/main.js
@@ -1,29 +1,34 @@
-$(function() {
-  $('a[href*="#"]:not([href="#"])').click(function() {
-    if (location.pathname.replace(/^\//,'') == this.pathname.replace(/^\//,'') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      target = target.length ? target : $('[name=' + this.hash.slice(1) +']');
-      if (target.length) {
-        $('html, body').animate({
-          scrollTop: target.offset().top
-        }, 1000);
-        return false;
-      }
-    }
-  });
-});
-
-$(document).ready(function(){
-  $('#carousel').slick({
-    accessibility: true,
-    dots:true,
-    prevArrow: $('.prev'),
-
-  });
-});
+//jQuery Smooth Scroll
 
 $(function() {
-  $('.menu-toggle').on('click', function(){
-    $('.mobile-menu').toggleClass('open');
-  });
+    $('a[href*="#"]:not([href="#"])').click(function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
+            if (target.length) {
+                $('html, body').animate({
+                    scrollTop: target.offset().top
+                }, 1000);
+                return false;
+            }
+        }
+    });
+});
+
+//Slick.JS
+
+$(document).ready(function() {
+    $('#carousel').slick({
+        accessibility: true,
+        dots: true,
+        prevArrow: $('.prev'),
+    });
+});
+
+//Mobile Navigation
+
+$(function() {
+    $('.menu-toggle').on('click', function() {
+        $('.mobile-menu').toggleClass('open');
+    });
 });
